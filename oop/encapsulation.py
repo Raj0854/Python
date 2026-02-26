240c0c72cf78477b11c7cd73c2ea2b10c0feb7b8
Raj gupta <rajgupta.02.0854@gmail.com>
2026-02-26 09:55:37 +0530

    Add files via upload

---
+++ oop/encapsulation.py
@@ -0,0 +1,20 @@
+class person:
+    def __init__(self, name, age):
+        self._name= name
+        self._age= age
+    def get_name(self):
+        return self._name
+    def set_name(self,new_name):
+        self._name = new_name
+    def get_age(self):
+        return self._age
+    def set_age(self,new_age):
+        if new_age >=0:
+            self._age = new_age
+        else:
+            print("age cannot be negative")
+
+Person = person("raj",21)
+print("Name :",person.get_name())
+print("Age :",person.get_age())
+ 
